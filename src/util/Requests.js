73c4d59cab64fb42416ae311b1bc1b157589ea65
Lee Adam <adam.lee@skywatch.com.tw>
2021-03-26 14:38:08 +0800

    feat: add timestamp

--- src/util/Requests.js
+++ src/util/Requests.js
@@ -5,12 +5,14 @@ import CoreManager from './CoreManager';
 const {API_KEY, REGION, SCOPE} = CoreManager;
 
 const getArchives = async (deviceId, archiveId, smartff) => {
+  const dateTime = Date.now();
+  const timestamp = Math.floor(dateTime / 1000);
   const res = await axios.get(
     `${
       Config.apiURL
     }/cameras/${deviceId}/archives/link?archive_id=${archiveId}&smart_ff=${smartff}&
     scope=${CoreManager.get(SCOPE)}&region=${CoreManager.get(REGION)}
-    &api_key=${CoreManager.get(API_KEY)}`,
+    &api_key=${CoreManager.get(API_KEY)}&_=${timestamp}`,
     {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
