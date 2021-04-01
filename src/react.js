6e4979d3864800e470d2d20f8ad13d1c8b6ee15f
mengpeilee <asdfg3865@gmail.com>
2021-04-01 11:45:19 +0800

    chor: package setting

--- src/react.js
+++ src/react.js
@@ -1,32 +1,11 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
 import ArchivesPlayer from './react/ArchivesPlayer';
 import FlvPlayer from './react/FlvPlayer';
 import {CoreManager} from './util';
 
 const {API_KEY} = CoreManager;
 
-CoreManager.set(API_KEY, 'c367498bf8adcb4b403d8b92b09fa948');
-
-const APP = () => {
-  const [player, setPlayer] = useState(null);
-
-  return (
-    // <FlvPlayer
-    //   deviceId={'47436'}
-    //   onPlayerInit={setPlayer}
-    //   onPlayerDispose={setPlayer}
-    //   style={{width: '768px', height: '432px'}}
-    // />
-    <ArchivesPlayer
-      onPlayerInit={setPlayer}
-      onPlayerDispose={setPlayer}
-      deviceId={'47436'}
-      archiveId={'124114069'}
-      smart_ff={0}
-      seek={0}
-    />
-  );
+const initialize = apiKey => {
+  CoreManager.set(API_KEY, apiKey);
 };
 
-ReactDOM.render(<APP />, document.getElementById('root'));
+export {initialize, ArchivesPlayer, FlvPlayer};
