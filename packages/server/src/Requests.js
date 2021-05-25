f3cbb142642233b53a2780d33a8b95961060b436
mengpeilee <asdfg3865@gmail.com>
2021-05-25 15:38:00 +0800

    fix: fix break line issue

--- packages/server/src/Requests.js
+++ packages/server/src/Requests.js
@@ -16,31 +16,29 @@ const getArchives = async (
 ) => {
   const dateTime = Date.now();
   const timestamp = Math.floor(dateTime / 1000);
-  const res = await axios.get(
-    `${config.apiURL}/cameras/${deviceId}/archives/link?
-    lang=${lang}&archive_id=${archiveId}&smart_ff=${smartff}&
-    scope=${coreManager.get(SCOPE)}&region=${coreManager.get(REGION)}&
-    api_key=${apiKey}&_=${timestamp}`,
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
+  const url = `${
+    config.apiURL
+  }/cameras/${deviceId}/archives/link?lang=${lang}&archive_id=${archiveId}&smart_ff=${smartff}&scope=${coreManager.get(
+    SCOPE,
+  )}&region=${coreManager.get(REGION)}&api_key=${apiKey}&_=${timestamp}`;
+
+  const res = await axios.get(url, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-  );
+  });
   return res;
 };
 
 const getFlvStream = async (deviceId, lang = default_lang, apiKey) => {
   const dateTime = Date.now();
   const timestamp = Math.floor(dateTime / 1000);
-  const res = await axios.get(
-    `${config.apiURL}/cameras/${deviceId}/flvstream?lang=${lang}&warmup=1&api_key=${apiKey}&_=${timestamp}`,
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
+  const url = `${config.apiURL}/cameras/${deviceId}/flvstream?lang=${lang}&warmup=1&api_key=${apiKey}&_=${timestamp}`;
+  const res = await axios.get(url, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-  );
+  });
   return res;
 };
 
