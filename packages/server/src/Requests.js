1f229335ceeb97602817285d5597c5fba1f2ffb9
mengpeilee <asdfg3865@gmail.com>
2021-05-25 15:29:30 +0800

    fix: fix typo

--- packages/server/src/Requests.js
+++ packages/server/src/Requests.js
@@ -17,11 +17,10 @@ const getArchives = async (
   const dateTime = Date.now();
   const timestamp = Math.floor(dateTime / 1000);
   const res = await axios.get(
-    `${
-      config.apiURL
-    }/cameras/${deviceId}/archives/link?lang=${lang}&archive_id=${archiveId}&smart_ff=${smartff}&
-    scope=${coreManager.get(SCOPE)}&region=${coreManager.get(REGION)}
-    &api_key=${apiKey}&_=${timestamp}`,
+    `${config.apiURL}/cameras/${deviceId}/archives/link?
+    lang=${lang}&archive_id=${archiveId}&smart_ff=${smartff}&
+    scope=${coreManager.get(SCOPE)}&region=${coreManager.get(REGION)}&
+    api_key=${apiKey}&_=${timestamp}`,
     {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -35,8 +34,7 @@ const getFlvStream = async (deviceId, lang = default_lang, apiKey) => {
   const dateTime = Date.now();
   const timestamp = Math.floor(dateTime / 1000);
   const res = await axios.get(
-    `${config.apiURL}/cameras/${deviceId}/flvstream?lang=${lang}&warmup=1
-    &api_key=${apiKey}&_=${timestamp}`,
+    `${config.apiURL}/cameras/${deviceId}/flvstream?lang=${lang}&warmup=1&api_key=${apiKey}&_=${timestamp}`,
     {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
