0ecf0e5d8bfd2d6d1e6755b9ce2b0e18a3949a49
mengpeilee <asdfg3865@gmail.com>
2021-05-25 10:48:30 +0800

    fix: fix typo

--- packages/api/src/Constants.js
+++ packages/api/src/Constants.js
@@ -1,6 +1,6 @@
 // Storage State
 const API_KEY = 'api_key';
-const SERVER_URL = 'client_server_rul';
+const SERVER_URL = 'client_server_url';
 const LANG_SELECTOR = 'lang';
 const SITE_URL = 'api_site_url';
 const REGION = 'region';
