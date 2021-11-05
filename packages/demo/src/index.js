bb3754310c686e692b4fb3ea4359817a790c579a
mengpeilee <asdfg3865@gmail.com>
2021-11-05 17:22:28 +0800

    feat: add new packages - demo

---
+++ packages/demo/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const APP = () => {
+  return <div>123</div>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <APP />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
