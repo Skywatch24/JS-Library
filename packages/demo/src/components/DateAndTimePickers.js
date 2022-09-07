92ef29c5b2558722e06e6f9668a8bac18b853f46
Ryoko.Li <ryoko.li@skywatch.com.tw>
2022-09-07 16:32:06 +0800

    chore: remove unused props and styles

--- packages/demo/src/components/DateAndTimePickers.js
+++ packages/demo/src/components/DateAndTimePickers.js
@@ -1,45 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {makeStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    width: 250,
-  },
-}));
-
-const DateAndTimePickers = ({id, label, value, disabled, onChange}) => {
-  const classes = useStyles();
-
+const DateAndTimePickers = ({id, label, disabled, onChange}) => {
   return (
-    <form className={classes.container} noValidate>
-      <TextField
-        id={id}
-        label={label}
-        type="datetime-local"
-        variant="outlined"
-        size="small"
-        margin="normal"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        disabled={disabled}
-        onChange={onChange}
-      />
-    </form>
+    <TextField
+      id={id}
+      label={label}
+      type="datetime-local"
+      variant="outlined"
+      size="small"
+      margin="normal"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      disabled={disabled}
+      onChange={onChange}
+    />
   );
 };
 
 DateAndTimePickers.defaultProps = {
   id: 'date-time-picker',
   label: '',
-  value: '',
   disabled: false,
   onChange: () => {},
 };
@@ -47,7 +30,6 @@ DateAndTimePickers.defaultProps = {
 DateAndTimePickers.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
   disabled: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
 };
