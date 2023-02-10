0df4d5e9ef59ef7478372fe8643dc5d088814c2c
Ryoko.Li <ryoko.li@skywatch.com.tw>
2023-02-10 10:50:38 +0800

    chore: remove unused code

--- packages/demo/src/steps/ReactCameraView.js
+++ packages/demo/src/steps/ReactCameraView.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
 import {CameraView} from '@skywatch/react';
 import {StepContainer, Subtitle, Description} from '../components';
